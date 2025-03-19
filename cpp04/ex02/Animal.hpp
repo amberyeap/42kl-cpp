0a9fa64513beb17605c8090f74ec8cb577ecd48d
amber <amberyeap002@gmail.com>
2025-03-19 17:10:58 +0800

    modified cpp04/ex02/Animal.hpp - wondering is #ifndef is needed in c++ header files

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ANIMAL_HPP
-# define ANIMAL_HPP
+// #ifndef ANIMAL_HPP
+// # define ANIMAL_HPP
 
 # include <iostream>
 
@@ -35,4 +35,4 @@ class Animal {
 
 };
 
-#endif
+// #endif
