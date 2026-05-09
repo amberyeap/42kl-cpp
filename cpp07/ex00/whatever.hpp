70aaf355dcda8acde8ed5cdcc44381905bfd8de7
amberyeap <amberyeap02@gmail.com>
2026-05-09 16:08:31 +0800

    added functions swap, min and max

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -15,4 +15,27 @@
 
 #include <iostream>
 
+template <typename T>
+void swap(T &x, T &y) {
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+T min(const T x, const T y) {
+	if (x == y)
+		return y;
+	if (x < y)
+		return x;
+	return y;
+}
+
+template <typename T>
+T max(const T x, const T y) {
+	if (x > y)
+		return x;
+	return y;
+}
+
 #endif
