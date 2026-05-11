fab83cae60374fa524196379a24c13d4858172b8
amberyeap <amberyeap02@gmail.com>
2026-05-12 02:08:03 +0800

    implemented the OCF and the adding numbers functions
    currently the addMultipleNums() only works for one type of container, vector

---
+++ cpp08/ex01/Span.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayeap <ayeap@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 01:40:11 by ayeap             #+#    #+#             */
+/*   Updated: 2026/05/12 01:40:11 by ayeap            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span() : _maxN(0), _data(NULL) {}
+
+Span::Span(unsigned int N) : _maxN(N), _data(NULL) {}
+
+Span::Span(const Span& other) : _maxN(other._maxN), _data(other._data) {}
+
+Span::~Span() {}
+
+Span& Span::operator=(const Span& other) {
+	if (this != &other) {
+		this->_maxN = other._maxN;
+		this->_data = other._data;
+	}
+	return *this;
+}
+
+void Span::addNumber(unsigned int n) {
+	if (_data.size() >= _maxN)
+		throw Span::ExceedLimit();
+	_data.push_back(n);
+}
+
+// how to make it accept other container types?
+void Span::addMultipleNums(std::vector<int> vect) {
+	if (_data.size() + vect.size() > _maxN)
+		throw Span::ExceedLimit();
+	_data.insert(_data.end(), vect.begin(), vect.end());
+}
