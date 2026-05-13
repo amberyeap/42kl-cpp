e144c7ea0fdb73fa0559319a8dab2beef7b54e40
amberyeap <amberyeap02@gmail.com>
2026-05-14 01:57:56 +0800

    committing current progress and still untested

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,14 +1,36 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack {
+#include <stack>
+#include <iostream>
 
+/* subclass std::stack - needed for this exercise 
+but discouraged overall because of the lack of virtual destructors */
+
+// std::stack has an underlying container which by default is std::deque
+
+template <typename T>
+class MutantStack : public std::stack<T> {
     public:
-        MutantStack();
-        MutantStack(const MutantStack& other);
-        ~MutantStack();
+        MutantStack() {}
+        MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+        ~MutantStack() {}
 
-        MutantStack& operator=(const MutantStack& other);
+        MutantStack& operator=(const MutantStack& other) {
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator begin() {
+			return this->c.begin();
+		}
+
+		iterator end() {
+			return this->c.end();
+		}
 };
 
 #endif
