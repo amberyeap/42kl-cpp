5087f7b6819a7ddb0e034066e855c84645cb7e8d
amberyeap <amberyeap02@gmail.com>
2026-05-18 17:58:11 +0800

    added fordJohnson() for vector

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -8,6 +8,14 @@ PmergeMe::~PmergeMe() {}
 
 PmergeMe& PmergeMe::operator=(const PmergeMe& other) { (void)other; return *this; }
 
+void PmergeMe::sort(std::vector<int>& arr) {
+	fordJohnson(arr);
+}
+
+// void PmergeMe::sort(std::deque<int>& arr) {
+// 	fordJohnson(arr);
+// }
+
 // use the jacobsthal algo to determine the order of numbers inserted
 // use binaryInsert algo to insert in the proper location
 
@@ -59,4 +67,52 @@ std::vector<int> PmergeMe::getJacobsthalOrder(size_t size) {
 	}
 
 	return order;
+}
+
+void PmergeMe::fordJohnson(std::vector<int>& arr) {
+	if (arr.size() <= 1)
+		return;
+
+	// base case for recursion
+	if (arr.size() == 2) {
+		if (arr[0] > arr[1])
+			std::swap(arr[0], arr[1]);
+		return;
+	}
+
+	bool hasLeftover = false;
+	int leftover;
+	if (arr.size() % 2 != 0) {
+		leftover = arr.back();
+		arr.pop_back();
+		hasLeftover = true;
+	}
+
+	std::vector<int> main;
+	std::vector<int> pend;
+
+	for (size_t i = 0; i < arr.size(); i += 2) {
+		if (arr[i] > arr[i + 1]) {
+			main.push_back(arr[i]);
+			pend.push_back(arr[i + 1]);
+		}
+		else {
+			main.push_back(arr[i + 1]);
+			pend.push_back(arr[i]);
+		}
+	}
+
+	// recursively sort main until reaches base case of 2 nums
+	fordJohnson(main);
+
+	main.insert(main.begin(), pend[0]);
+
+	std::vector<int> order = getJacobsthalOrder(pend.size());
+	for (size_t i = 0; i < order.size(); i++)
+		binaryInsert(main, pend[order[i]]);
+
+	if (hasLeftover)
+		binaryInsert(main, leftover);
+
+	arr = main;
 }
