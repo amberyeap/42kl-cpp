cad756b01df2a4f8780dd91bc01366c61b806da1
amberyeap <amberyeap02@gmail.com>
2026-05-18 17:20:20 +0800

    added jacobsthal algo

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -26,3 +26,37 @@ void PmergeMe::binaryInsert(std::vector<int>& arr, int val) {
 	arr.insert(arr.begin() + low, val);
 }
 
+std::vector<int> PmergeMe::getJacobsthalOrder(size_t size) {
+	std::vector<int> jacob;
+	jacob.push_back(1);
+	jacob.push_back(3);
+	while (jacob.back() < (int)size)
+		jacob.push_back(jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2]);
+
+	std::vector<int> order;
+	std::vector<bool> added(size, false);
+
+	for (size_t i = 0; i < jacob.size(); i++) {
+		int end = std::min((int)size - 1, jacob[i] - 1);
+		int start;
+
+		if (i == 0)
+			start = 1;
+		else
+			start = jacob[i - 1];
+
+		for (int j = end; j >= start; j--) {
+			if (j < (int)size && !added[j]) {
+				order.push_back(j);
+				added[j] = true;
+			}
+		}
+	}
+
+	for (size_t i = 1; i < size; i++) {
+		if (!added[i])
+			order.push_back(i);
+	}
+
+	return order;
+}
