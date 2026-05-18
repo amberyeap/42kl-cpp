aed35294974931808cab5fe45fd242d69534e1e2
amberyeap <amberyeap02@gmail.com>
2026-05-18 17:00:34 +0800

    added binaryInsert() function

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -2,8 +2,27 @@
 
 PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(const PmergeMe& other) {}
+PmergeMe::PmergeMe(const PmergeMe& other) { (void)other; }
 
 PmergeMe::~PmergeMe() {}
 
-PmergeMe& PmergeMe::operator=(const PmergeMe& other) {}
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) { (void)other; return *this; }
+
+// use the jacobsthal algo to determine the order of numbers inserted
+// use binaryInsert algo to insert in the proper location
+
+// insert the chosen one into the right spot
+void PmergeMe::binaryInsert(std::vector<int>& arr, int val) {
+	int low = 0;
+	int high = arr.size();
+
+	while (low < high) {
+		int mid = (low + high) / 2;
+		if (arr[mid] < val)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	arr.insert(arr.begin() + low, val);
+}
+
